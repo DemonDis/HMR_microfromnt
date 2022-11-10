b04b13884bf8b541eb8e61ef796a8a0061479418
DemonDis <50297797+DemonDis@users.noreply.github.com>
2022-11-11 00:18:49 +0700

    hot react reload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HMR_microfromnt
+
+https://github.com/gaearon/react-hot-loader/issues/1150
---
+++ packages/app-1/src/BtnTest.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const component = () => (
+  <div className="container">
+    BtnTest
+  </div>
+);
+
+export default component;
